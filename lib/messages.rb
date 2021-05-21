056b24a6bf93f4306018a66cf4a417741548333f
Alexandra Kaudy <lex.court.k@gmail.com>
2021-05-21 16:44:57 -0600

    Add partially finished instructions

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,2 +1,20 @@
 class Messages
+
+  def welcome
+    p "Welcome to MASTERMIND.
+      Do you want to (#{p})lay, read the (#{i})nstructions,
+      or (#{q})uit?"
+  end
+
+  def i
+    # instructions
+    p "The computer will generate a Master Code containing a randomized
+    combination of each of these four colors."
+    p "There are four different colors: (r)ed,
+      (g)reen, (b)lue, and (y)ellow."
+    p "In order to win, you must guess Master Code (the correct colors in the
+      correct order) within 10 attempts. Colors may or may not be repeated in
+      the Master Code."
+    p ""
+  end
 end
